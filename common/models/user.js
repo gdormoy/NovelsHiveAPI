8b64fcf61516a266653956dd9bf113319b348f7e
Vincent Guénin <guenin.vincent4@gmail.com>
2019-05-23 21:43:12 +0200

    Add email verification on user registration

--- common/models/user.js
+++ common/models/user.js
@@ -1,5 +1,20 @@
 'use strict';
 
 module.exports = function(User) {
+  User.afterRemote('create', function(context, userInstance, next) {
+    let options = {
+      type: 'email',
+      to: userInstance.email,
+      from: 'noreply@novelshive.com',
+      subject: 'Thanks for registering.',
+      user: User
+    };
 
+    userInstance.verify(options, function(err, response, next) {
+      if (err) return next(err);
+      console.log('> verification email sent');
+    });
+
+    next();
+  });
 };
