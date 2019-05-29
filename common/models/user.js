7a7198752fcbd8286e60054d5531e7b89d12a152
Vincent Guénin <guenin.vincent4@gmail.com>
2019-05-29 09:56:54 +0200

    [UPD] user confirmation email body

--- common/models/user.js
+++ common/models/user.js
@@ -7,13 +7,6 @@ let path = require('path');
 module.exports = function(User) {
   User.afterRemote('create', function(context, userInstance, next) {
     // eslint-disable-next-line max-len
-    const emailText = 'Thanks for registering. Please follow the link below to complete your registration.\n\n' +
-      '[MUST INSERT THE LINK]'; // TODO : Add this in config
-
-    const emailBody = {
-      heading: 'Welcome to Novels Hive :)',
-      text: emailText,
-    };
 
     let options = {
       type: 'email',
@@ -24,7 +17,7 @@ module.exports = function(User) {
       user: User,
       redirect: app.get('novelshiveWebsite') + '/login',
       heading: 'Welcome to Novels Hive :)',
-      text: emailText,
+      host: app.get('applicationURL'),
     };
 
     userInstance.verify(options, function(err, response, next) {
