92d7dea62ff237c80484aa7259af55ef9f0ca716
dormoy <dormoy.guillaume@gmail.com>
2019-07-03 22:27:22 +0200

    add remote method to find favorite

--- common/models/favorite.js
+++ common/models/favorite.js
@@ -1,5 +1,24 @@
 'use strict';
 
 module.exports = function(Favorite) {
+  Favorite.beforeRemote('create', function(context, favoritInstance, next) {
+    let requestParams = context.args.data;
 
+    let requestFilter = {
+      'where': {
+        'userId': requestParams.userId,
+        'storyId': requestParams.storyId
+      }
+    };
+
+    Favorite.findOne(requestFilter, function (err, instance) {
+      if (instance.length > 0) {
+        let error = new Error('User have not the story in favoris');
+        error.statusCode = 404;
+        return next(error);
+      } else {
+        next()
+      }
+    });
+  });
 };
