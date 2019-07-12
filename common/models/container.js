94b62904355d321e189a71f69b2f4767145ad2ee
Vincent Guénin <guenin.vincent4@gmail.com>
2019-07-12 08:21:41 +0200

    [ADD] Custom route to get file

--- common/models/container.js
+++ common/models/container.js
@@ -1,5 +1,33 @@
 'use strict';
+let fs = require('fs');
 
 module.exports = function(Container) {
+  Container.getFileContent = function(container, file, cb) {
+    let path = 'storage/images/' + container + '/' + file;
+    let mimeType = 'image/' + file.slice(file.lastIndexOf('.') + 1);
+    console.log(mimeType);
 
+    fs.open(path, 'r', (err, fd) => {
+      let fileStat = fs.statSync(path);
+
+      let content = Buffer.alloc(fileStat.size);
+      fs.read(fd, content, 0, content.length, 0, (err, bytesRead, buffer) => {
+        cb(null, {
+          mimeType: mimeType,
+          size: bytesRead,
+          content: buffer
+        });
+      });
+    })
+  };
+
+  Container.remoteMethod('getFileContent', {
+    accepts: [
+      {arg: 'container', type: 'string', http: {source: 'path'}, required: true, description: 'Name of the container'},
+      {arg: 'file', type: 'string', http: {source: 'path'}, required: true, description: 'Name of the file'}
+    ],
+    returns: {arg: 'result', type: 'string'},
+    http: {path: '/:container/files/:file/read', verb: 'GET'},
+    description: 'Récupère le fichier'
+  });
 };
