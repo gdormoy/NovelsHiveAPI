42187f2185d0717b205fc9675eb4cb03a1b1e035
Vincent Guénin <guenin.vincent4@gmail.com>
2019-07-14 16:16:24 +0200

    [ADD] POST /beta_readers returns user

--- common/models/beta-reader.js
+++ common/models/beta-reader.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = function(Betareader) {
-
+  Betareader.afterRemote('create', function (context, betareaderInstance, next) {
+    Betareader.app.models.user.findById(context.result.userId, {}, function (err, instance) {
+      // context.result['user'] = instance;
+      context.result = {
+        userId: context.result.userId,
+        id: context.result.id,
+        storyId: context.result.storyId,
+        user: instance
+      };
+      next();
+    })
+  })
 };
